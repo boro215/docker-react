7137bca4572a914d976ffe7518172efe6ff64abc
boro215 <borys.piotr.hawrylec@gmail.com>
2025-03-03 18:16:24 +0100

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import ReactDOM from 'react-dom';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders withot crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
